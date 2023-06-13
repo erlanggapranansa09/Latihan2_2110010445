87295c2ed8219436c4cd38d9b79580b953024e0a
erlanggapranansa09 <pranansaerlangga@gmail.com>
2023-06-13 08:43:38 +0700

    Melengkapi Class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -6,5 +6,28 @@ public class Mahasiswa {
     private String nama, prodi;
     protected int npm;
     
+     public void setnama(String nama) {
+        this.nama = nama;
+    }
+    
+     public void setprodi(String prodi) {
+        this.prodi = prodi;
+    }
      
+     public void setnpm(int npm) {
+        this.npm = npm;
+    } 
+     
+     public String getnama() {
+         return nama;
+     }
+     
+     public String getprodi() {
+         return prodi;
+     }
+     
+     public int getnpm() {
+         System.out.println("superclass");
+         return npm;
+     }
 }
